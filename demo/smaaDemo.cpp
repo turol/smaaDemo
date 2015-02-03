6635db09b614f2fc5702d4088f8f85c91370a8c8
Turo Lamminen <turotl@gmail.com>
2015-02-03 15:09:22 +0200

    Fix degree/radian confusion

--- demo/smaaDemo.cpp
+++ demo/smaaDemo.cpp
@@ -747,8 +747,8 @@ void SMAADemo::render() {
 	uint32_t ticks = SDL_GetTicks();
 	const uint32_t rotationPeriod = 30 * 1000;
 	ticks = ticks % rotationPeriod;
-	glm::mat4 view = glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -25.0f)), float(-360.0f * ticks) / rotationPeriod, glm::vec3(0.0f, 1.0f, 0.0f));
-	glm::mat4 proj = glm::perspective(65.0f, float(windowWidth) / windowHeight, 0.1f, 100.0f);
+	glm::mat4 view = glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -25.0f)), float(M_PI * 2.0f * ticks) / rotationPeriod, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 proj = glm::perspective(float(65.0f * M_PI * 2.0f / 360.0f), float(windowWidth) / windowHeight, 0.1f, 100.0f);
 	glm::mat4 viewProj = proj * view;
 	glUniformMatrix4fv(viewProjLoc, 1, GL_FALSE, glm::value_ptr(viewProj));
 
