358253a98c092896bdb374ef758610c5ca712753
Turo Lamminen <turotl@gmail.com>
2015-09-14 12:18:53 +0300

    Add emscripten build config

---
+++ emscripten/example.mk
@@ -0,0 +1,39 @@
+# example of local configuration
+# copy to local.mk
+
+
+# location of source
+TOPDIR:=..
+
+
+LTO:=n
+ASAN:=n
+TSAN:=n
+UBSAN:=n
+
+
+# compiler options etc
+CC:=emcc
+CXX:=em++
+CFLAGS:=-g -Wall -Wextra -Werror -Wshadow
+CFLAGS+=-Wno-unused-local-typedefs
+OPTFLAGS:=-O3
+
+
+# lazy assignment because CFLAGS is changed later
+CXXFLAGS=$(CFLAGS)
+CXXFLAGS+=-std=c++11
+
+
+LDFLAGS:=-g
+LDFLAGS+=-s TOTAL_MEMORY=67108864
+LDFLAGS+=-s OUTLINING_LIMIT=5000
+LDLIBS:=
+
+
+LTOCFLAGS:=--llvm-lto 3
+LTOLDFLAGS:=--llvm-lto 3
+
+
+OBJSUFFIX:=.o
+EXESUFFIX:=.js
