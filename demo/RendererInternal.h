951280d5ed332e6a44f563eefcc099a12ad3f91d
Turo Lamminen <turotl@gmail.com>
2017-07-11 18:44:36 +0300

    Add ResourceContainer::get which takes a Handle

--- demo/RendererInternal.h
+++ demo/RendererInternal.h
@@ -40,8 +40,16 @@ public:
 	}
 
 
+	// TODO: remove
 	const T &get(unsigned int handle) const {
 		auto it = resources.find(handle);
+		assert(it != resources.end());
+
+		return it->second;
+	}
+
+	const T &get(Handle<T> handle) const {
+		auto it = resources.find(handle.handle);
 		assert(it != resources.end());
 
 		return it->second;
