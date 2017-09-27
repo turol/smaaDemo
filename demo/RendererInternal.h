d77a0a3ae68ebfa0dd7bd3df365d956984bbd60e
Turo Lamminen <turotl@gmail.com>
2017-09-27 13:49:37 +0300

    Move Includer to RendererCommon

--- demo/RendererInternal.h
+++ demo/RendererInternal.h
@@ -99,40 +99,6 @@ public:
 #endif  // RENDERER
 
 
-class Includer final : public shaderc::CompileOptions::IncluderInterface {
-	std::unordered_map<std::string, std::vector<char> > cache;
-
-
-	virtual shaderc_include_result* GetInclude(const char* requested_source, shaderc_include_type /* type */, const char* /* requesting_source */, size_t /* include_depth */) {
-		std::string filename(requested_source);
-
-		// std::unordered_map<std::string, std::vector<char> >::iterator it = cache.find(filename);
-		auto it = cache.find(filename);
-		if (it == cache.end()) {
-			auto contents = readFile(requested_source);
-			bool inserted = false;
-			std::tie(it, inserted) = cache.emplace(std::move(filename), std::move(contents));
-			// since we just checked it's not there this must succeed
-			assert(inserted);
-		}
-
-		auto data = new shaderc_include_result;
-		data->source_name         = it->first.c_str();
-		data->source_name_length  = it->first.size();
-		data->content             = it->second.data();
-		data->content_length      = it->second.size();
-		data->user_data           = nullptr;
-
-		return data;
-	}
-
-	virtual void ReleaseInclude(shaderc_include_result* data) {
-		// no need to delete any of data's contents, they're owned by this class
-		delete data;
-	}
-};
-
-
 struct RendererImpl : public RendererBase {
 	SwapchainDesc swapchainDesc;
 
