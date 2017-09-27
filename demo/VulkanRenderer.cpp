355382d009bc9c8ade2a4fe4eeadd308447c8594
Turo Lamminen <turotl@gmail.com>
2017-09-27 16:13:03 +0300

    32-bit Vulkan fix

--- demo/VulkanRenderer.cpp
+++ demo/VulkanRenderer.cpp
@@ -131,7 +131,7 @@ RendererBase::~RendererBase()
 
 
 static VkBool32 VKAPI_PTR debugCallbackFunc(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType, uint64_t object, size_t location, int32_t /* messageCode */, const char * pLayerPrefix, const char * pMessage, void * /* pUserData*/) {
-	printf("layer %s %s object %lu type %s location %lu: %s\n", pLayerPrefix, vk::to_string(vk::DebugReportFlagBitsEXT(flags)).c_str(), static_cast<unsigned long>(object), vk::to_string(vk::DebugReportObjectTypeEXT(objectType)).c_str(), location, pMessage);
+	printf("layer %s %s object %lu type %s location %lu: %s\n", pLayerPrefix, vk::to_string(vk::DebugReportFlagBitsEXT(flags)).c_str(), static_cast<unsigned long>(object), vk::to_string(vk::DebugReportObjectTypeEXT(objectType)).c_str(), static_cast<unsigned long>(location), pMessage);
 	// make errors fatal
 	abort();
 
