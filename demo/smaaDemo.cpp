a5d30d8d812b296192bd88da26da8795edc736c9
Turo Lamminen <turotl@gmail.com>
2017-09-29 15:53:50 +0300

    Fix Imgui special key handling

--- demo/smaaDemo.cpp
+++ demo/smaaDemo.cpp
@@ -789,7 +789,7 @@ void SMAADemo::initRender() {
 	{
 		ImGuiIO& io = ImGui::GetIO();
 		io.IniFilename                 = nullptr;
-		io.KeyMap[ImGuiKey_Tab]        = SDLK_TAB;                     // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
+		io.KeyMap[ImGuiKey_Tab]        = SDL_SCANCODE_TAB;                     // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
 		io.KeyMap[ImGuiKey_LeftArrow]  = SDL_SCANCODE_LEFT;
 		io.KeyMap[ImGuiKey_RightArrow] = SDL_SCANCODE_RIGHT;
 		io.KeyMap[ImGuiKey_UpArrow]    = SDL_SCANCODE_UP;
@@ -798,16 +798,16 @@ void SMAADemo::initRender() {
 		io.KeyMap[ImGuiKey_PageDown]   = SDL_SCANCODE_PAGEDOWN;
 		io.KeyMap[ImGuiKey_Home]       = SDL_SCANCODE_HOME;
 		io.KeyMap[ImGuiKey_End]        = SDL_SCANCODE_END;
-		io.KeyMap[ImGuiKey_Delete]     = SDLK_DELETE;
-		io.KeyMap[ImGuiKey_Backspace]  = SDLK_BACKSPACE;
-		io.KeyMap[ImGuiKey_Enter]      = SDLK_RETURN;
-		io.KeyMap[ImGuiKey_Escape]     = SDLK_ESCAPE;
-		io.KeyMap[ImGuiKey_A]          = SDLK_a;
-		io.KeyMap[ImGuiKey_C]          = SDLK_c;
-		io.KeyMap[ImGuiKey_V]          = SDLK_v;
-		io.KeyMap[ImGuiKey_X]          = SDLK_x;
-		io.KeyMap[ImGuiKey_Y]          = SDLK_y;
-		io.KeyMap[ImGuiKey_Z]          = SDLK_z;
+		io.KeyMap[ImGuiKey_Delete]     = SDL_SCANCODE_DELETE;
+		io.KeyMap[ImGuiKey_Backspace]  = SDL_SCANCODE_BACKSPACE;
+		io.KeyMap[ImGuiKey_Enter]      = SDL_SCANCODE_RETURN;
+		io.KeyMap[ImGuiKey_Escape]     = SDL_SCANCODE_ESCAPE;
+		io.KeyMap[ImGuiKey_A]          = SDL_SCANCODE_A;
+		io.KeyMap[ImGuiKey_C]          = SDL_SCANCODE_C;
+		io.KeyMap[ImGuiKey_V]          = SDL_SCANCODE_V;
+		io.KeyMap[ImGuiKey_X]          = SDL_SCANCODE_X;
+		io.KeyMap[ImGuiKey_Y]          = SDL_SCANCODE_Y;
+		io.KeyMap[ImGuiKey_Z]          = SDL_SCANCODE_Z;
 
 		// TODO: clipboard
 		io.SetClipboardTextFn = nullptr;
