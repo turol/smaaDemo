82c550dae8e519c287de8e92598b0fcd3aff93aa
Tuomas Närväinen <tuomas.narvainen@alternativegames.net>
2017-10-17 16:17:39 +0300

    Fix VS2015 build.

--- foreign/pcg-cpp/include/pcg_random.hpp
+++ foreign/pcg-cpp/include/pcg_random.hpp
@@ -1203,7 +1203,10 @@ public:
         return baseclass::period_pow2() + table_size*extvalclass::period_pow2();
     }
 
-    __attribute__((always_inline)) result_type operator()()
+#ifdef __GNUC__
+    __attribute__((always_inline))
+#endif
+			result_type operator()()
     {
         result_type rhs = get_extended_value();
         result_type lhs = this->baseclass::operator()();
