426cd9cbced3c86ed1f2956fb93bf8c0a1e02ace
Turo Lamminen <turotl@gmail.com>
2017-10-17 13:00:40 +0300

    Use full PCG generator

--- demo/smaaDemo.cpp
+++ demo/smaaDemo.cpp
@@ -36,6 +36,8 @@ THE SOFTWARE.
 
 #include <tclap/CmdLine.h>
 
+#include <pcg_random.hpp>
+
 #include "Renderer.h"
 #include "Utils.h"
 
@@ -120,25 +122,8 @@ void SetClipboardText(void* /* user_data */, const char* text) {
 }
 
 
-// *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
-// Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
-
-typedef struct { uint64_t state;  uint64_t inc; } pcg32_random_t;
-
-static uint32_t pcg32_random_r(pcg32_random_t* rng)
-{
-    uint64_t oldstate = rng->state;
-    // Advance internal state
-    rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
-    // Calculate output function (XSH RR), uses old state for max ILP
-    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
-    uint32_t rot = oldstate >> 59u;
-    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
-}
-
-
 class RandomGen {
-	pcg32_random_t rng;
+	pcg32 rng;
 
 	RandomGen(const RandomGen &) = delete;
 	RandomGen &operator=(const RandomGen &) = delete;
@@ -148,11 +133,8 @@ class RandomGen {
 public:
 
 	explicit RandomGen(uint64_t seed)
+	: rng(seed)
 	{
-		rng.state = seed;
-		rng.inc = 1;
-		// spin it once for proper initialization
-		pcg32_random_r(&rng);
 	}
 
 
@@ -165,7 +147,7 @@ public:
 
 
 	uint32_t randU32() {
-		return pcg32_random_r(&rng);
+		return rng();
 	}
 };
 
