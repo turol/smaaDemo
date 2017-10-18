f1a8ba1b76ec71ab5f9e65b561a70bcb68f29dd3
Turo Lamminen <turotl@gmail.com>
2017-10-18 15:23:00 +0300

    Store near plane distance in a variable

--- demo/smaaDemo.cpp
+++ demo/smaaDemo.cpp
@@ -1290,10 +1290,11 @@ void SMAADemo::render() {
 
 		// TODO: better calculation, and check cube size (side is sqrt(3) currently)
 		float farPlane = cameraDistance + 4.0f * float(cubesPerSide);
+		float nearPlane = 0.1f;
 
 		glm::mat4 model = glm::rotate(glm::mat4(1.0f), cameraRotation, glm::vec3(0.0f, 1.0f, 0.0f));
 		glm::mat4 view = glm::lookAt(glm::vec3(cameraDistance, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		glm::mat4 proj = glm::perspective(float(65.0f * M_PI * 2.0f / 360.0f), float(windowWidth) / windowHeight, 0.1f, farPlane);
+		glm::mat4 proj = glm::perspective(float(65.0f * M_PI * 2.0f / 360.0f), float(windowWidth) / windowHeight, nearPlane, farPlane);
 		globals.viewProj = proj * view * model;
 
 		renderer.setViewport(0, 0, windowWidth, windowHeight);
