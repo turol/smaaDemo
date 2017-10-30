81c735386011d1738a256bbb06e416c89dacf6cf
Turo Lamminen <turotl@gmail.com>
2017-10-30 15:58:36 +0200

    Remove a mingw hack

--- demo/VulkanMemoryAllocator.cpp
+++ demo/VulkanMemoryAllocator.cpp
@@ -23,30 +23,12 @@ THE SOFTWARE.
 
 #ifdef RENDERER_VULKAN
 
+#include <mutex>
+
 
 #if defined(__GNUC__) && defined(_WIN32)
 
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif  // WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-
-
-// mingw doesn't have std::mutex, must define this ourselves
-
-   class VmaMutex
-   {
-   public:
-       VmaMutex() { }
-       ~VmaMutex() { }
-       void Lock() { EnterCriticalSection(&cs); }
-       void Unlock() { LeaveCriticalSection(&cs); }
-   private:
-       CRITICAL_SECTION cs;
-   };
-   #define VMA_MUTEX VmaMutex
+#include <mingw.mutex.h>
 
 #endif  // defined(__GNUC__) && defined(_WIN32)
 
