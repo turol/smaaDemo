11031c734fe30c0533c2a0d587f273f6e9ea7bc2
Turo Lamminen <turotl@gmail.com>
2018-10-03 16:34:01 +0300

    Whitespace

--- renderer/RendererCommon.cpp
+++ renderer/RendererCommon.cpp
@@ -497,16 +497,16 @@ std::vector<uint32_t> RendererBase::compileSpirv(const std::string &name, const 
 			options.AddMacroDefinition(p.first, p.second);
 		}
 
-	shaderc_shader_kind kind;
-	switch (kind_) {
-	case ShaderKind::Vertex:
-		kind = shaderc_glsl_vertex_shader;
-		break;
+		shaderc_shader_kind kind;
+		switch (kind_) {
+		case ShaderKind::Vertex:
+			kind = shaderc_glsl_vertex_shader;
+			break;
 
-	case ShaderKind::Fragment:
-		kind = shaderc_glsl_fragment_shader;
-		break;
-	}
+		case ShaderKind::Fragment:
+			kind = shaderc_glsl_fragment_shader;
+			break;
+		}
 
 		shaderc::Compiler compiler;
 		auto result = compiler.CompileGlslToSpv(&src[0], src.size(), kind, name.c_str(), options);
