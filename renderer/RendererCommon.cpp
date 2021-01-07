5f756e28dc8102e1e600615e57ca282e04d33d7f
Turo Lamminen <turol@iki.fi>
2021-01-07 12:25:05 +0200

    Update SPIRV headers and tools to deal with breaking changes

--- renderer/RendererCommon.cpp
+++ renderer/RendererCommon.cpp
@@ -483,7 +483,7 @@ std::vector<char> RendererBase::loadSource(const std::string &name) {
 
 // increase this when the shader compiler options change
 // so that the same source generates a different SPV
-const unsigned int shaderVersion = 61;
+const unsigned int shaderVersion = 62;
 
 
 struct CacheData {
