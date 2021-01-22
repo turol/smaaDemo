586ea53e1b150368b52be6f630421b2bad76013e
Turo Lamminen <turol@iki.fi>
2021-01-22 16:10:23 +0200

    Switch from ShaderC to glslang only

--- renderer/RendererCommon.cpp
+++ renderer/RendererCommon.cpp
@@ -21,7 +21,7 @@ THE SOFTWARE.
 */
 
 
-#define USE_SHADERC 1
+// #define USE_SHADERC 1
 
 
 #include "RendererInternal.h"
@@ -488,7 +488,7 @@ std::vector<char> RendererBase::loadSource(const std::string &name) {
 
 // increase this when the shader compiler options change
 // so that the same source generates a different SPV
-const unsigned int shaderVersion = 78;
+const unsigned int shaderVersion = 79;
 
 
 struct CacheData {
