2241db72dcb1c4e2fb2c95d66e9a9d4e98af62b7
Turo Lamminen <turol@iki.fi>
2022-11-10 11:36:05 +0200

    Fix unused variable non-debug builds

--- renderer/RendererCommon.cpp
+++ renderer/RendererCommon.cpp
@@ -940,7 +940,7 @@ compilationNeeded:
 			cacheData.spirvHash = hash;
 			cacheData.includes  = includer.included;
 
-			auto inserted = shaderCache.emplace(cacheKey, std::move(cacheData));
+			auto DEBUG_ASSERTED inserted = shaderCache.emplace(cacheKey, std::move(cacheData));
 			assert(inserted.second);
 		}
 
