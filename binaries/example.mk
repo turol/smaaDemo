8bcedcfe11311c1ac4b5b4f9e1b0778b144d1c60
Turo Lamminen <turol@iki.fi>
2023-06-02 13:46:02 +0300

    Enable extra semicolon warning

--- binaries/example.mk
+++ binaries/example.mk
@@ -36,6 +36,7 @@ WIN32:=n
 
 CFLAGS:=-g -Wall -Wextra -Werror -Wshadow
 CFLAGS+=-Wno-unused-local-typedefs
+CFLAGS+=-Wextra-semi
 CFLAGS+=-Wzero-as-null-pointer-constant
 CFLAGS+=$(shell sdl2-config --cflags)
 OPTFLAGS:=-O
